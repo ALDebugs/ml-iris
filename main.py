d6f0f3ecc84561ffc31a8aafdd8f773341ca7cd2
ALDebugs <203330425+ALDebugs@users.noreply.github.com>
2025-05-03 14:08:28 +0100

    Models set up Replaced "multi-class" with "OneVsRestClassifier" for LogisticRegression -> deprecated

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -49,3 +50,38 @@ plt.show()
 scatter_matrix(dataset)
 plt.show()
 # Diagonal grouping of pairs of attributes -> high correlation and predictable relationship
+############################################################
+
+# Validation Dataset
+# Split-out validation dataset: split into two -> 80% used for training, eval, select among the models
+# 20% held back as a validation dataset
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+"""
+	Stratified 10-fold cross validation estimate model accuracy.
+	Dataset split in 10, 9 for training, 1 for testing -> repeated for all combinations of train-test splits.
+	Stratified = each fold/split of the dataset aims for the same distribution of example by class as exists in the whole training dataset.
+	Fixed random seed, so all algorithms train on the same dataset splits.
+"""
+
+# Spot Check Algorithms
+models = []
+models.append(("LR", OneVsRestClassifier(LogisticRegression(solver="liblinear"))))
+models.append(("LDA", LinearDiscriminantAnalysis()))
+models.append(("KNN", KNeighborsClassifier()))
+models.append(("CART", DecisionTreeClassifier()))
+models.append(("NB", GaussianNB()))
+models.append(("SVM", SVC(gamma="auto")))
+
+# Evaluate each model
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring="accuracy")
+	results.append(cv_results)
+	names.append(name)
+	print("%s: %f (%f)" % (name, cv_results.mean(), cv_results.std()))
