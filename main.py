b628d7f0fe001bac9ee801c77d240ab87ada61a6
ALDebugs <203330425+ALDebugs@users.noreply.github.com>
2025-05-03 14:21:37 +0100

    Tutorial complete (machinelearningmastery.com) with comparisons, predictions and evaluations

--- main.py
+++ main.py
@@ -68,13 +68,14 @@ X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=
 """
 
 # Spot Check Algorithms
-models = []
-models.append(("LR", OneVsRestClassifier(LogisticRegression(solver="liblinear"))))
-models.append(("LDA", LinearDiscriminantAnalysis()))
-models.append(("KNN", KNeighborsClassifier()))
-models.append(("CART", DecisionTreeClassifier()))
-models.append(("NB", GaussianNB()))
-models.append(("SVM", SVC(gamma="auto")))
+models = [
+	("LR", OneVsRestClassifier(LogisticRegression(solver="liblinear"))),
+	("LDA", LinearDiscriminantAnalysis()),
+	("KNN", KNeighborsClassifier()),
+	("CART", DecisionTreeClassifier()),
+	("NB", GaussianNB()),
+	("SVM", SVC(gamma="auto"))
+		  ]
 
 # Evaluate each model
 results = []
@@ -85,3 +86,18 @@ for name, model in models:
 	results.append(cv_results)
 	names.append(name)
 	print("%s: %f (%f)" % (name, cv_results.mean(), cv_results.std()))
+
+# Compare Algorithms - using box and whisker plots
+plt.boxplot(results, tick_labels=names)
+plt.title("Algorithm Comparison")
+plt.show()
+
+# Make predictions on validation dataset
+model = SVC(gamma="auto")
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
